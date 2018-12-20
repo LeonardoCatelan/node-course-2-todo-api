a7f7cba08cb43f51d65d6fc60f1874221769458e
LeonardoCatelan <44733148+LeonardoCatelan@users.noreply.github.com>
2018-12-20 09:19:41 -0200

    auth for users post

--- server/server.js
+++ server/server.js
@@ -103,6 +103,23 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    
+
+    user.save().then(() => {
+        user.generateAuthToken();
+        // res.send(user);        
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => res.status(400).send(e));0
+})
+
+
 app.listen(port, () => {
     console.log(`Started on port: ${port}`);
 })
